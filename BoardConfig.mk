6f0492c46de609d3ff94e31826b54c7777bd4cbf
doye <74832250+ryxxqxosy@users.noreply.github.com>
2025-01-24 11:47:39 +0900

    Delete BoardConfig.mk

--- BoardConfig.mk
+++
@@ -1,7 +0,0 @@
-# ==== SELinux Configurations ====
-# Enable SEPolicy directories for custom Firefox rules and additional SEPolicy paths
-BOARD_SEPOLICY_DIRS += \
-    device/lenovo/Tab_P11/sepolicy \
-    external/sepolicy \
-    device/generic/goldfish/sepolicy/common
-
